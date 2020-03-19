099068f4f64a6ca698cd0ad126853def6ed33c43
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-19 01:13:36 -0500

    Refactor users routes to use a usersRouter.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { parse } = require('json2csv');
 const js2xmlparser = require('js2xmlparser');
 
-const users = require('./fixtures/users');
+const usersRouter = require('./routes/users');
 const emails = require('./fixtures/emails');
 
 const app = express();
@@ -11,16 +11,7 @@ app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/users', (req, res, next) => {
-  // Note that when the Accept header is '*/*', it will match whatever req.accepts() call you have. So, if you want to return JSON as the default, you have to set it as the first option. The browser (at least Chrome) sends the following Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9. Since the */* is in there, the JSON will get returned instead of the csv or xml.
-  if (req.accepts('application/json')) return res.json(users);
-  if (req.accepts('text/csv')) {
-    return res.type('text/csv').send(convertToCSV(users));
-  } 
-  if (req.accepts('application/xml')) {
-    return res.type('application/xml').send(js2xmlparser.parse('users', users));
-  }
-});
+app.use('/users', usersRouter);
 
 app.get('/emails', (req, res, next) => {
   if (req.accepts('application/json')) return res.json(emails);
