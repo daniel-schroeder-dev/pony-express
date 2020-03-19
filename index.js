4013adc29b15fe6a9ebb78af1d0be2ab8987233a
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-18 21:14:41 -0500

    Setup basic GET routes for /users and /emails that return JSON.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const users = require('./fixtures/users');
+const emails = require('./fixtures/emails');
 
 const app = express();
+
+app.get('/users', (req, res, next) => {
+  res.json(users);
+});
+
+app.get('/emails', (req, res, next) => {
+  res.json(emails);
+});
 
 app.listen(3000, () => {
   console.log('Express app up at 3000.');
