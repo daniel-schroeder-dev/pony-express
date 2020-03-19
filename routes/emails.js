09e69916de26dacd77ea93382824b488dbf0b77e
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-19 01:41:22 -0500

    Refactor formatResponse to use arrow functions.

--- routes/emails.js
+++ routes/emails.js
@@ -1,18 +1,11 @@
 const express = require('express');
-const convertToCSV = require('../utils/convertToCSV');
-const js2xmlparser = require('js2xmlparser');
 const emails = require('../fixtures/emails');
+const formatResponse = require('../utils/formatResponse');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  if (req.accepts('application/json')) return res.json(emails);
-  if (req.accepts('text/csv')) {
-    return res.type('text/csv').send(convertToCSV(emails));
-  }
-  if (req.accepts('application/xml')) {
-    return res.type('application/xml').send(js2xmlparser.parse('emails', emails));
-  }
+  formatResponse(res, emails, 'emails');
 });
 
 module.exports = router;
--- utils/formatResponse.js
+++ utils/formatResponse.js
@@ -1,20 +1,12 @@
 const convertToCSV = require('../utils/convertToCSV');
 const js2xmlparser = require('js2xmlparser');
 
-function formatResponse(res, data, xmlRoot) {
+const formatResponse = (res, data, xmlRoot) => {
   res.format({
-    'application/json': function () {
-      res.json(data);
-    },
-
-    'text/csv': function () {
-      res.send(convertToCSV(data));
-    },
-
-    'application/xml': function () {
-      res.send(js2xmlparser.parse(xmlRoot, data));
-    },
+    'application/json': () => res.json(data),
+    'text/csv': () => res.send(convertToCSV(data)),
+    'application/xml': () => res.send(js2xmlparser.parse(xmlRoot, data)),
   });
-}
+};
 
 module.exports = formatResponse
