0fb5aa89ecac562e373d350e9cbc49488c295896
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-18 22:29:02 -0500

    Add heroku config.

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const emails = require('./fixtures/emails');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/users', (req, res, next) => {
   // Note that when the Accept header is '*/*', it will match whatever req.accepts() call you have. So, if you want to return JSON as the default, you have to set it as the first option. The browser (at least Chrome) sends the following Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9. Since the */* is in there, the JSON will get returned instead of the csv or xml.
   if (req.accepts('application/json')) return res.json(users);
@@ -28,8 +30,8 @@ app.get('/emails', (req, res, next) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Express app up at 3000.');
+app.listen(PORT, () => {
+  console.log('Express app up at:', PORT);
 });
 
 function convertToCSV(jsonData) {
