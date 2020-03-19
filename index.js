28e4d3d9721bdda4ae5113b2249d89ca98460f97
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-18 21:32:18 -0500

    Return csv at GET /users when Accept 'text/csv' header is present.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { parse } = require('json2csv');
 const users = require('./fixtures/users');
 const emails = require('./fixtures/emails');
 
@@ -6,7 +7,14 @@ const app = express();
 
 app.get('/users', (req, res, next) => {
   if (req.accepts('text/csv')) {
-    console.log('csv');
+    const userFields = Object.keys(users[0]);
+    const userOpts = { userFields };
+    try {
+      const csv = parse(users, userOpts);
+      return res.send(csv);
+    } catch (err) {
+      console.error(err);
+    }
   }
   res.json(users);
 });
