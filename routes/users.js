311c62060f1b4f154ce19552c20b5402c453bf69
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-19 01:29:27 -0500

    Use res.format() in usersRouter to perform content-negotiation.

--- routes/users.js
+++ routes/users.js
@@ -6,14 +6,19 @@ const users = require('../fixtures/users');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  // Note that when the Accept header is '*/*', it will match whatever req.accepts() call you have. So, if you want to return JSON as the default, you have to set it as the first option. The browser (at least Chrome) sends the following Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9. Since the */* is in there, the JSON will get returned instead of the csv or xml.
-  if (req.accepts('application/json')) return res.json(users);
-  if (req.accepts('text/csv')) {
-    return res.type('text/csv').send(convertToCSV(users));
-  } 
-  if (req.accepts('application/xml')) {
-    return res.type('application/xml').send(js2xmlparser.parse('users', users));
-  }
+  res.format({
+    'application/json': function () {
+      res.json(users);
+    },
+
+    'text/csv': function () {
+      res.send(convertToCSV(users));
+    },
+
+    'application/xml': function () {
+      res.send(js2xmlparser.parse('users', users));
+    },
+  });
 });
 
 module.exports = router;
