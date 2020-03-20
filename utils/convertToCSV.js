988d9f6de21773d6e65a12953dd19718c2cc9647
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-19 19:37:01 -0500

    Add duck-typing in convertToCSV to handle object or array as input.

--- utils/convertToCSV.js
+++ utils/convertToCSV.js
@@ -1,14 +1,20 @@
 const { parse } = require('json2csv');
 
 const convertToCSV = (jsonData) => {
-  const fields = Object.keys(jsonData[0]);
+
+  // jsonData could be an array or an object, just need the object to pull the keys out of to populate the 'fields' variable for json2csv's parse() call.
+  jsonData = jsonData.length ? jsonData[0] : jsonData;
+  
+  const fields = Object.keys(jsonData);
   const opts = { fields };
+  
   try {
     const csv = parse(jsonData, opts);
     return csv;
   } catch (err) {
     console.error(err);
   }
+
 };
 
 module.exports = convertToCSV;
