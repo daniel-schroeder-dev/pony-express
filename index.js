72bd11b0aaff5b08d8c31d1fd00cc29ca4da43b3
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-23 16:15:23 -0500

    Create middleware folder and make a logger module. Add logger module to global app middleware.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const chalk = require('chalk');
+
+const logger = require('./middleware/logger');
 
 const usersRouter = require('./routes/users');
 const emailsRouter = require('./routes/emails');
@@ -7,37 +8,7 @@ const emailsRouter = require('./routes/emails');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use((req, res, next) => {
-  const { method, url } = req;
-  res.on('finish', () => {
-    console.log(formatLogString(method, url, res.statusCode));
-  });
-  next();
-});
-
-const formatLogString = (method, url, statusCode) => {
-  method = addColorToMethodString(method);
-  statusCode = addColorToStatusCode(statusCode);
-  const tabs = url.length < 8 ? '\t\t' : '\t';
-  return `${method}\t${url}${tabs}${statusCode}`;
-};
-
-const addColorToMethodString = method => {
-  const methodsToColors = {
-    'GET': chalk.magentaBright,
-    'POST': chalk.greenBright,
-    'PATCH': chalk.yellowBright,
-    'DELETE': chalk.redBright,
-  };
-  return methodsToColors[method](method);
-};
-
-const addColorToStatusCode = statusCode => {
-  if (statusCode <= 299) return chalk.greenBright(statusCode);
-  if (statusCode <= 399) return chalk.yellowBright(statusCode);
-  if (statusCode <= 499) return chalk.redBright(statusCode);
-  return chalk.bgRedBright(statusCode);
-};
+app.use(logger);
 
 app.use(express.static('public'));
 
