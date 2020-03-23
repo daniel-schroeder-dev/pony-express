a1f9f6ccb5f5508522d73ddf3cdc1047112f2592
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-23 16:04:13 -0500

    Create addColorToMethodString for coloring log output.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const chalk = require('chalk');
+
 const usersRouter = require('./routes/users');
 const emailsRouter = require('./routes/emails');
 
@@ -8,10 +10,24 @@ const app = express();
 app.use((req, res, next) => {
   const { method, url } = req;
   res.on('finish', () => {
-    console.log(`${method} ${url} ${res.statusCode}`);
+    console.log(`${addColorToMethodString(method)} ${url} ${res.statusCode}`);
   });
   next();
 });
+
+const formatLogString = (method, url, statusCode) => {
+  
+};
+
+const addColorToMethodString = method => {
+  const methodsToColors = {
+    'GET': chalk.magentaBright,
+    'POST': chalk.greenBright,
+    'PATCH': chalk.yellowBright,
+    'DELETE': chalk.redBright,
+  };
+  return methodsToColors[method](method);
+};
 
 app.use(express.static('public'));
 
