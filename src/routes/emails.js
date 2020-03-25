9c35075282c9fc31bb68560a48309cdf897bb9d9
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-25 12:17:39 -0500

    Add body-parser package and replace custom jsonBodyParser middleware with it in emails routes.

--- src/routes/emails.js
+++ src/routes/emails.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const jsonBodyParser = require('../middleware/jsonBodyParser');
+const jsonParser = require('body-parser').json();
 const { getEmails, getEmail, deleteEmail, postEmail, patchEmail } = require('./handlers/emails');
 
 const router = express.Router();
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/', getEmails);
 router.get('/:id', getEmail);
 router.delete('/:id', deleteEmail);
-router.post('/', jsonBodyParser, postEmail);
-router.patch('/:id', jsonBodyParser, patchEmail);
+router.post('/', jsonParser, postEmail);
+router.patch('/:id', jsonParser, patchEmail);
 
 module.exports = router;
