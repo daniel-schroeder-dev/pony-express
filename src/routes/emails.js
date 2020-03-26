fccdf3f5cce8106ec9b06e529ac23eff482b85df
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-26 14:20:07 -0500

    Add middleware to parse urlEncoded request bodies to POST/PATCH routes in emails router.

--- src/routes/emails.js
+++ src/routes/emails.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const jsonParser = require('body-parser').json();
+const urlEncodedParser = require('body-parser').urlencoded({ extended: true });
 const upload = require('multer')({ dest: path.join(__dirname, '../../uploads/' )});
 
 const { getEmails, getEmail, deleteEmail, postEmail, patchEmail } = require('./handlers/emails');
@@ -11,7 +12,7 @@ const router = express.Router();
 router.get('/', getEmails);
 router.get('/:id', getEmail);
 router.delete('/:id', deleteEmail);
-router.post('/', jsonParser, upload.array('attachment'), postEmail);
-router.patch('/:id', jsonParser, patchEmail);
+router.post('/', urlEncodedParser, jsonParser, upload.array('attachment'), postEmail);
+router.patch('/:id', urlEncodedParser, jsonParser, patchEmail);
 
 module.exports = router;
