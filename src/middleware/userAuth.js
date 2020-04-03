f976d2d23aa66af36942965ee789589956e4fda2
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-03 13:40:49 -0500

    Create _validateUserCredentials() helper function.

--- src/middleware/userAuth.js
+++ src/middleware/userAuth.js
@@ -1,23 +1,14 @@
 const NotFoundError = require('../errors/NotFoundError');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
-const users = require('../fixtures/users');
-
 const userAuth = (req, res, next) => {
-  
-  const authHeader = req.get('Authorization');
-  
-  _parseAuthHeader(authHeader, res);
-  
-  const userCredentials = Buffer.from(authHeader.split(' ')[1], 'base64').toString('utf8');
-  const [ username, password ] = userCredentials.split(':');
-  const user = users.find(user => user.username === username && user.password === password);
-  
-  if (!user) throw new NotFoundError('No user found with given username and password');
 
-  req.user = user;
+  const authHeader = req.get('Authorization');
+  _parseAuthHeader(authHeader, res);
+  req.user = _validateUserCredentials(authHeader);
   
   next();
+
 };
 
 const _parseAuthHeader = (authHeader, res) => {
@@ -25,6 +16,20 @@ const _parseAuthHeader = (authHeader, res) => {
     res.set('WWW-Authenticate', 'Basic: realm="Access to user account"');
     throw new UnauthorizedError('Must provide a valid username and password')
   }
+};
+
+const _validateUserCredentials = authHeader => {
+
+  const users = require('../fixtures/users');
+
+  const userCredentials = Buffer.from(authHeader.split(' ')[1], 'base64').toString('utf8');
+  const [ username, password ] = userCredentials.split(':');
+  const user = users.find(user => user.username === username && user.password === password);
+  
+  if (!user) throw new NotFoundError('No user found with given username and password');
+
+  return user;
+
 };
 
 module.exports = userAuth;
