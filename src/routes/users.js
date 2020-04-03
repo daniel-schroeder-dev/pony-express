77559e54209116c6261ca76b2300fce9d5f4ada3
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-03 12:42:10 -0500

    Remove GET /users.
    There's no reason to have a route where all of the users are returned.

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,10 +7,6 @@ const users = require('../fixtures/users');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  formatResponse(res, users, 'users');
-});
-
 router.get('/:id', (req, res, next) => {
   const user = users.find(user => user.id === req.params.id);
   if (!user) throw new NotFoundError('No user found with id: ' + req.params.id);
