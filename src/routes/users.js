89a4fefb782bdf60a36df4d94f6fbee52870aebd
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-03 12:49:32 -0500

    Begin setup of Basic Authentication middleware.
    Setup check for 'Authorization' header and returned a challenge
    if it isn't present. The values from the 'Authorization' header are
    not being validated yet.

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,7 +7,17 @@ const users = require('../fixtures/users');
 
 const router = express.Router();
 
-router.get('/:id', (req, res, next) => {
+const userAuth = (req, res, next) => {
+  const authHeader = req.get('Authorization');
+  if (!authHeader) {
+    res.set('WWW-Authenticate', 'Basic: realm="Access to user account"');
+    res.status(401).json({ msg: 'Must provide a valid username and password' });
+  } else {
+    next();
+  }
+};
+
+router.get('/:id', userAuth, (req, res, next) => {
   const user = users.find(user => user.id === req.params.id);
   if (!user) throw new NotFoundError('No user found with id: ' + req.params.id);
   formatResponse(res, user, 'user');
