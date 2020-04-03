72d4c9da0762100ea5d0e849513c9dbbcc607551
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-03 14:52:09 -0500

    Return all emails from GET /emails csv.
    Bug fix, I was only returning the first email as a CSV because
    I passed the wrong data into the convertToCSV function.

--- src/utils/convertToCSV.js
+++ src/utils/convertToCSV.js
@@ -3,9 +3,9 @@ const { parse } = require('json2csv');
 const convertToCSV = (jsonData) => {
 
   // jsonData could be an array or an object, just need the object to pull the keys out of to populate the 'fields' variable for json2csv's parse() call.
-  jsonData = jsonData.length ? jsonData[0] : jsonData;
+  const jsonDataFields = jsonData.length ? jsonData[0] : jsonData;
   
-  const fields = Object.keys(jsonData);
+  const fields = Object.keys(jsonDataFields);
   const opts = { fields };
   
   try {
