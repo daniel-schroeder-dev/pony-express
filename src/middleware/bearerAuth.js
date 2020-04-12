44a0a543d1ed4e8d0633a1fceb2ee981d0326460
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-12 15:47:29 -0500

    Ensure Bearer Authentication header is present in bearerAuth middleware.

--- src/middleware/bearerAuth.js
+++ src/middleware/bearerAuth.js
@@ -1,10 +1,19 @@
 const users = require('../fixtures/users');
+const UnauthorizedError = require('../errors/UnauthorizedError');
 
 const bearerAuth = (req, res, next) => {
-  
+
+  validateAuthHeader(req.get('Authorization'), res);
 
   // req.user = users.find(user => user.email)
   next();
+};
+
+const validateAuthHeader = (authHeader, res) => {
+  if (!authHeader || authHeader.split(' ')[0] !== 'Bearer') {
+    res.set('WWW-Authenticate', 'Bearer realm="Access to email server"');
+    throw new UnauthorizedError('Must provide valid Bearer authentication credentials');
+  }
 };
 
 module.exports = bearerAuth;
